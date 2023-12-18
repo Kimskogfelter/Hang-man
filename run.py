778e92b1f0d14048d1ea42f0322bfb489dddc3aa
Kimskogfelter <>
2023-12-18 14:15:51 +0000

    moved parts of the code to the bottom so functions are defined before they are called

--- run.py
+++ run.py
@@ -12,29 +12,15 @@ random_word_easy = random.choice(words_easy)
 random_word_medium = random.choice(words_medium)
 random_word_hard = random.choice(words_hard)
 
-# variables for the different difficulties that select
-# the correct function
-Easy = easy_difficulty()
-Medium = medium_difficulty()
-Hard = hard_difficulty()
-
-# prints out the initial strings when the game starts
-print('Hi and welcome to hangman.')
-print('You got 3 different difficulties to choose from:')
-print('Easy, Medium and Hard.')
-print('You got 6 tries to guess the correct word.')
-print('   ',  '------')
-print('   ',  '|    |')
-print('   ',  '|     ')
-print('   ',  '|     ')
-print('   ',  '|     ')
-print('   ',  '|     ')
-print('--------------')
-difficulty = input("Please choose a degree of difficulty: ")
-
-print('Let me think of a word...')
+# variables that will store the correct and wrong guesses
+correct_guess_easy = ['_'] * len(random_word_easy)
+correct_guess_medium = ['_'] * len(random_word_medium)
+correct_guess_hard = ['_'] * len(random_word_hard)
+wrong_guess = []
 
 # function that makes the user wait between choices
+
+
 def waiting_time():
     for i in range(5):
         print('.', end="")
@@ -44,14 +30,10 @@ def waiting_time():
 
 waiting_time()
 
-# variables that will store the correct and wrong guesses
-correct_guess_easy = ['_'] * len(random_word_easy)
-correct_guess_medium = ['_'] * len(random_word_medium)
-correct_guess_hard = ['_'] * len(random_word_hard)
-wrong_guess = []
-
 # function that prints out the different parts of the hang man
 # when the user guesses a wrong letter
+
+
 def hangman_parts(x):
     if x == 0:
         print('   ',  '------')
@@ -181,12 +163,14 @@ def easy_difficulty():
             print('You lose!')
             print(f'The correct word was {random_word_easy}')
             break
-        # checks if all the letters are correct and prints the winning statement
+        # prints the winning statement
         if '_' not in correct_guess_easy:
             print('You won!')
             break
 
 # main function for MEDIUM difficulty
+
+
 def medium_difficulty():
     # prints out how many letters the choosen word has
     print('The word has', len(random_word_medium), 'letters')
@@ -236,12 +220,14 @@ def medium_difficulty():
             print('Game Over. You lose!')
             print(f'The correct word was {random_word_medium}')
             break
-        # checks if all the letters are correct and prints the winning statement
+        # prints the winning statement
         if '_' not in correct_guess_medium:
             print('You won! Congratulations!')
             break
 
 # main function for HARD difficulty
+
+
 def hard_difficulty():
     # prints out how many letters the choosen word has
     print('The word has', len(random_word_hard), 'letters')
@@ -292,7 +278,40 @@ def hard_difficulty():
             print('Game Over. You lose!')
             print(f'The correct word was {random_word_hard}')
             break
-        # checks if all the letters are correct and prints the winning statement
+        # prints the winning statement
         if '_' not in correct_guess_hard:
             print('You won! Congratulations!')
             break
+
+
+# prints out the initial strings when the game starts
+print('Hi and welcome to hangman.')
+print('You got 3 different difficulties to choose from:')
+print('Easy, medium and hard.')
+print('You got 6 tries to guess the correct word.')
+print('   ',  '------')
+print('   ',  '|    |')
+print('   ',  '|     ')
+print('   ',  '|     ')
+print('   ',  '|     ')
+print('   ',  '|     ')
+print('--------------')
+# Ask the user to choose a difficulty
+difficulty = input("Please choose a degree of difficulty "
+                   "('easy', 'medium', or 'hard'): ")
+
+# Map difficulty names to their corresponding functions
+difficulty_functions = {
+    'easy': easy_difficulty,
+    'medium': medium_difficulty,
+    'hard': hard_difficulty
+}
+
+# Check if the entered difficulty is valid
+chosen_difficulty_function = difficulty_functions.get(difficulty)
+if chosen_difficulty_function:
+    print(f'Let me think of a {difficulty} word...')
+    chosen_difficulty_function()  # Call the chosen difficulty function
+else:
+    print("Invalid difficulty.")
+    print("Please choose either 'easy', 'medium', or 'hard'.")
